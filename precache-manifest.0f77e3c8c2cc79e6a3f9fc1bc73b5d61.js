823d48be0cafcfb684dcbd155785a716f0f039fb
maks <MaksP21@gmail.com>
2020-05-03 13:23:28 +0300

    Updates

---
+++ precache-manifest.0f77e3c8c2cc79e6a3f9fc1bc73b5d61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472ff4289cf8f44647dcdf1b3d28f84f",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "c9027a95d7b21ff2d789",
+    "url": "/social_network/static/css/main.a1546158.chunk.css"
+  },
+  {
+    "revision": "7763b49700cda6900475",
+    "url": "/social_network/static/js/2.c0b07445.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network/static/js/2.c0b07445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9027a95d7b21ff2d789",
+    "url": "/social_network/static/js/main.8e09b28c.chunk.js"
+  },
+  {
+    "revision": "f878a9441b1b054edd6b",
+    "url": "/social_network/static/js/runtime-main.eb638784.js"
+  },
+  {
+    "revision": "945f33b643f2ceffcdae90fb57c61854",
+    "url": "/social_network/static/media/631929649c.945f33b6.png"
+  }
+]);
